39241330e88ee238bc63e9b67026eed977faa81c
Connor Meth <connormeth27@gmail.com>
2019-09-09 20:17:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,6 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
   
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+
+def using_unshift(bouroughs_in_nyc, )
