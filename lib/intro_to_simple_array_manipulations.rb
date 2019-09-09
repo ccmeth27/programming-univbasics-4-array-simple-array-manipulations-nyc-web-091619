5543932ff0c71de3a64690f37b3ed5a8e5741804
Connor Meth <connormeth27@gmail.com>
2019-09-09 20:43:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,7 @@ end
 dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
 
 def pop_with_args(dog_breeds)
-  pop_with_args.pop(3, 4)
+  small_dogs = pop_with_args.pop(3, 4)
 end
 
 
