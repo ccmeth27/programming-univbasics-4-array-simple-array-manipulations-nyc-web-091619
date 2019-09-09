0b04b6a3797931a6327cf89a89a9182df1dfc5a8
Connor Meth <connormeth27@gmail.com>
2019-09-09 20:16:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 
 def using_push(colors_in_the_rainbow, next_color)
-  colors_in_the_rainbow.push(next_color)
+  colors_in_the_rainbow.push("violet")
 end
   
