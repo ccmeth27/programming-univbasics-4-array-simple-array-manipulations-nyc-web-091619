3e7ce604ae7b79189803daf0a330b49dfcedecf6
Connor Meth <connormeth27@gmail.com>
2019-09-09 20:55:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,10 @@ def using_shift(my_favorite_cities)
   im_so_over_this_city = my_favorite_cities.shift 
 end
 
+ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
 
+def shift_with_args(ice_cream_brands)
+  
 
 
 
