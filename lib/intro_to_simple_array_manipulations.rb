ed045a205e9d8f1427b104f6a0ca1c1445c3a919
Connor Meth <connormeth27@gmail.com>
2019-09-09 20:58:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,8 @@ end
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
 def using_concat(my_favorite_things, more_favs)
-  all_my_favs 
+  all_my_favs = my_favorite_things.concat(more_favs)
+end
 
 
 
